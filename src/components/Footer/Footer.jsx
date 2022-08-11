81017e3499a089cc557ff67c9b5667a085537582
JasonxWarren <warrenjason1@gmail.com>
2022-08-11 01:29:11 -0400

    Updates

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return(
-        <footer className="footer container">
-        <span className="footer__copy"> &#169; Jason Warren's Portfolio </span>
-        </footer>
-    )
-}
-
-export default Footer
